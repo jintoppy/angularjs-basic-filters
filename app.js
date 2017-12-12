2bdf804bfc3b3b992e884bd192dc72ebba905a91
Jinto Jose <jintoppy@gmail.com>
2017-12-12 12:49:24 +0530

    custom filter

--- app.js
+++ app.js
@@ -4,6 +4,18 @@ angular.module('myapp')
     .component('app', {
         templateUrl: 'app.component.html',
         controller: function(){
+            this.greeting = 'Hello Everyone';
 
+            this.dob = '1513061958000';
+            
         }
+    });
+
+angular.module('myapp')
+    .filter('myName', function(){
+
+        return function(value, greeting){
+            return greeting + " filtered value is " + value;
+        }; 
+
     });
